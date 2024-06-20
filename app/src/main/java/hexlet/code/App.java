3e4c21ae991ad079944684df17dbf7048c96e773
Aivaseda <aivaseda@bk.ru>
2024-06-20 06:32:52 +0500

    add json file, getData

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,36 +1,39 @@
 package hexlet.code;
+
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
 
-import java.util.concurrent.Callable;
+
 
 @Command(name = "gendiff", version = "App 2.0", mixinStandardHelpOptions = true,
         description = "Compares two configuration files and shows a difference.")
 public class App implements Runnable {
+
     @Parameters(paramLabel = "filePath1", description = "path to first file")
-    private static String first;
+    String first;
 
     @Parameters(paramLabel = "filePath2", description = "path to second file")
-    private static String second;
+    String second;
 
     @Option(names = { "-f", "--format" }, description = "output format [default: stylish]", defaultValue = "stylish")
     private String format;
 
     @Override
-    public void run() {
-    }
-
-    public static void main(String[] args) {
-
-        var diff = Differ.generate(first, second);
+    public void run(){
+        String diff = null;
+        try {
+            diff = Differ.generate(first, second);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         System.out.println(diff);
-        int exitCode = new CommandLine(new App()).execute(args);
-        System.exit(exitCode);
     }
-
-
-
+    public static void main(String[] args) throws Exception {
+        new CommandLine(new App()).execute(args);
+        //System.exit(exitCode);
+    }
 }
+
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,7 +1,25 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.nio.file.Paths;
+import java.util.Map;
+
 public class Differ {
-    public static String generate(String oneP, String twoP ) {
+    public static String generate(String firstLink, String secondLink ) throws Exception {
+        var map1 = getData(firstLink);
+        var map2 = getData(secondLink);
+
         return "this work";
+
+    }
+    public static Map getData(String link) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        Map<?, ?> map = mapper.readValue(Paths.get(link).toFile(), Map.class);
+
+//        for (Map.Entry<?, ?> entry : map.entrySet()) {
+//            System.out.println(entry.getKey() + "=" + entry.getValue());
+//        }
+        return map;
     }
 }
+
