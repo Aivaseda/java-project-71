d102cbcddfb3d62d44a73e9359439910463057ca
Aivaseda <aivaseda@bk.ru>
2024-06-21 04:54:22 +0500

    added map sorting

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -3,22 +3,28 @@ package hexlet.code;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.nio.file.Paths;
 import java.util.Map;
+import java.util.TreeMap;
+
 
 public class Differ {
     public static String generate(String firstLink, String secondLink ) throws Exception {
         var map1 = getData(firstLink);
         var map2 = getData(secondLink);
 
-        return "this work";
+        return "generated map!";
 
     }
     public static Map getData(String link) throws Exception {
         ObjectMapper mapper = new ObjectMapper();
         Map<?, ?> map = mapper.readValue(Paths.get(link).toFile(), Map.class);
+        Map<?, ?> sortedTreeMap = new TreeMap<>(map);
 
-//        for (Map.Entry<?, ?> entry : map.entrySet()) {
-//            System.out.println(entry.getKey() + "=" + entry.getValue());
-//        }
+
+        for (Map.Entry<?, ?> entry : sortedTreeMap.entrySet()) {
+            System.out.println(entry.getKey() + "=" + entry.getValue());
+        }
+        System.out.println();
+
         return map;
     }
 }
