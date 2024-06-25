794ed0ab1484e87677f5f66934f12b5254862bc1
Aivaseda <aivaseda@bk.ru>
2024-06-26 03:43:05 +0500

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,7 @@ repositories { mavenCentral() }
 application { mainClass.set("hexlet.code.App") }
 
 dependencies {
+    implementation("org.jacoco:jacoco-maven-plugin:0.8.12")
     implementation ("info.picocli:picocli:4.7.6")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
     annotationProcessor ("info.picocli:picocli-codegen:4.7.6")
