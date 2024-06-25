565ac10848fabae3cd76fc98392032a2e37b71ed
Aivaseda <aivaseda@bk.ru>
2024-06-26 02:54:45 +0500

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,16 +11,17 @@ plugins {
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
-repositories {
-    mavenCentral()
-}
+repositories { mavenCentral() }
+
+application { mainClass.set("hexlet.code.App") }
 
 dependencies {
+    implementation ("info.picocli:picocli:4.7.6")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+    annotationProcessor ("info.picocli:picocli-codegen:4.7.6")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation ("info.picocli:picocli:4.7.6")
-    annotationProcessor ("info.picocli:picocli-codegen:4.7.6")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
 }
 
 tasks.test {
@@ -29,14 +30,11 @@ tasks.test {
     testLogging {
         exceptionFormat = TestExceptionFormat.FULL
         events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
-        // showStackTraces = true
+        showStackTraces = true
         // showCauses = true
         showStandardStreams = true
     }
 }
 
-application {
-    mainClass.set("hexlet.code.App")
-}
 
 tasks.jacocoTestReport { reports { xml.required.set(true) } }
